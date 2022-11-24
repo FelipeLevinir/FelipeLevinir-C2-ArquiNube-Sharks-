27360becef43de16e6f2b02986bbf789f386df7e
FelipeLevinir <levinir.f@gmail.com>
2022-11-23 22:20:44 -0300

    Correciones varias

--- node_project/views/js/code.js
+++ node_project/views/js/code.js
@@ -10,9 +10,9 @@ const on = (element, event, selector, handler) => {
 
 on(document, 'click', '.btnEditar', e =>{
     const fila = e.target.parentNode.parentNode
-    //id_editar.value = fila.children[0].innerHTML
-    name_editar.value = fila.children[0].innerHTML
-    character_editar.value = fila.children[1].innerHTML
+    id_editar.value = fila.children[1].innerHTML
+    name_editar.value = fila.children[2].innerHTML
+    character_editar.value = fila.children[3].innerHTML
     modalShark.show()
 });
 
